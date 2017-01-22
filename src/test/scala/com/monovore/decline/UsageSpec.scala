7fa227675841ea48480ec0dad3adfcf7c0826a9d
Ben Kirwin <ben@kirw.in>
2017-01-22 18:08:48 -0500

    Test fixup

--- src/test/scala/com/monovore/decline/UsageSpec.scala
+++ src/test/scala/com/monovore/decline/UsageSpec.scala
@@ -1,6 +1,5 @@
 package com.monovore.decline
 
-import cats.data.NonEmptyList
 import org.scalatest.{Matchers, WordSpec}
 
 class UsageSpec extends WordSpec with Matchers {
@@ -8,12 +7,12 @@ class UsageSpec extends WordSpec with Matchers {
   "Usage" should {
 
     "handle no opts" in {
-      Usage.fromOpts(Opts.apply(15)) should equal(NonEmptyList.of(Usage()))
+      Usage.fromOpts(Opts.apply(15)) should equal(List(Usage()))
     }
 
     "handle a single argument" in {
-      val usage = Usage.fromOpts(Opts.option[Int]("foo", "..."))
-      println(usage)
+      val usage = Usage.fromOpts(Opts.option[Int]("foo", "...")).flatMap { _.show }
+      usage should equal(List("--foo <integer>"))
     }
   }
 }
