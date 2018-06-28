4b8e8828e441d58f19143e85e66869cba0732195
Ben Kirwin <ben@kirw.in>
2018-06-27 22:00:35 -0400

    Add a benchmark for a bad performance issue

---
+++ bench/src/main/scala/com/monovore/decline/bench/OptionSequence.scala
@@ -0,0 +1,26 @@
+package com.monovore.decline.bench
+
+import com.monovore.decline._
+import org.openjdk.jmh.annotations.Benchmark
+import cats.implicits._
+
+object OptionSequence {
+
+  val n = 20
+
+  val command = Command("demo", "") {
+    (1 to n).map { i =>
+      Opts.option[String](s"opt$i", "").withDefault(s"$i")
+    }.reduceRight { (a, b) => (a, b).mapN(_ + _) }
+  }
+}
+
+class OptionSequence {
+
+  import OptionSequence._
+
+  @Benchmark
+  def run(): Unit = {
+    command.parse(Seq())
+  }
+}
