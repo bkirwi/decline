629a1c134af6730e1bcef7f47d6b8e661fb4e198
P. Oscar Boykin <boykin@pobox.com>
2020-07-17 14:25:49 -1000

    try to test another branch of the code

--- core/shared/src/test/scala/com/monovore/decline/ArgumentSpec.scala
+++ core/shared/src/test/scala/com/monovore/decline/ArgumentSpec.scala
@@ -14,6 +14,10 @@ class ArgumentSpec extends ArgumentSuite {
       Argument[Int]
     }
     assert(cnt == 0)
+
+    val ai2 = Defer[Argument].defer(ai)
+
+    // ai is evaluated first
     assert(ai.read("42").toOption == Some(42))
     assert(cnt == 1)
     assert(ai.read("314").toOption == Some(314))
@@ -21,9 +25,32 @@ class ArgumentSpec extends ArgumentSuite {
 
     assert(ai.defaultMetavar == Argument[Int].defaultMetavar)
 
-    // test defer two layers deep
-    assert(Defer[Argument].defer(ai).read("271").toOption == Some(271))
+    // now test a2 which is a defer of a defer
+    assert(ai2.read("271").toOption == Some(271))
     assert(cnt == 1)
+  }
+
+  test("check defer (nesting)") {
+    var cnt = 0
+    val ai = Defer[Argument].defer {
+      cnt += 1
+      Argument[Int]
+    }
+    assert(cnt == 0)
+
+    val ai2 = Defer[Argument].defer(ai)
+
+    // first test a2 which is a defer of a defer
+    assert(ai2.read("271").toOption == Some(271))
+
+    assert(cnt == 1)
+    // ai is evaluated second, ai2 should have triggered it
+    assert(ai.read("42").toOption == Some(42))
+    assert(cnt == 1)
+    assert(ai.read("314").toOption == Some(314))
+    assert(cnt == 1)
+
+    assert(ai.defaultMetavar == Argument[Int].defaultMetavar)
   }
 
   checkArgument[String]("String")
